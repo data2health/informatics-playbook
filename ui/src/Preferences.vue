74d66fbf7cd18294b1f0132ee109e190332a5460
sengineer0 <sengineer020@gmail.com>
2021-01-11 01:36:49 +0200

    extended the preferences function

--- ui/src/Preferences.vue
+++ ui/src/Preferences.vue
@@ -22,7 +22,7 @@
         </div>
       </div>
       <span class="menu-title">
-        Size
+        Font size
       </span>
       <div class="menu-item-wrapper-row">
         <div class="flex center-items" style="width: 50%; border-right: 1px solid #d6d6d6; cursor:pointer;"
@@ -32,6 +32,28 @@
         <div class="flex center-items" style="width: 50%; cursor:pointer;"
         v-on:click="increaseFontSize">
           A
+        </div>
+      </div>
+      <span class="menu-title">
+        Size
+      </span>
+      <div class="menu-item-wrapper-row">
+        <div class="flex center-items" style="width: 50%; border-right: 1px solid #d6d6d6; cursor:pointer;" v-on:click="decreaseDocumentSize">
+          <svg style="height: 22px; width: 22px;" xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="512" viewBox="0 0 64 64" width="512"><path d="m52.586 8.586-14.586 14.586v-5.172c0-1.104-.896-2-2-2s-2 .896-2 2v10c0 1.104.896 2 2 2h10c1.104 0 2-.896 2-2s-.896-2-2-2h-5.172l14.586-14.586c.781-.781.781-2.047 0-2.828s-2.047-.781-2.828 0z"/><path d="m10 56c.512 0 1.023-.195 1.414-.586l14.586-14.586v5.172c0 1.104.896 2 2 2s2-.896 2-2v-10c0-1.104-.896-2-2-2h-10c-1.104 0-2 .896-2 2s.896 2 2 2h5.172l-14.586 14.586c-.781.781-.781 2.047 0 2.828.391.391.902.586 1.414.586z"/></svg>
+        </div>
+        <div class="flex center-items" style="width: 50%; cursor:pointer;"  v-on:click="increaseDocumentSize">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512; height:15px; width:25px;" xml:space="preserve">
+          <g>
+            <g>
+              <path d="M492,0H344.212c-11.046,0-20,8.954-20,20s8.954,20,20,20h99.503L283.394,200.322c-7.811,7.811-7.811,20.474,0,28.284    c7.81,7.81,20.473,7.811,28.284,0L472,68.284v99.503c0,11.046,8.954,20,20,20c11.046,0,20-8.954,20-20V20    C512,9.115,503.154,0,492,0z"/>
+            </g>
+          </g>
+          <g>
+            <g>
+              <path d="M228.606,283.394c-7.811-7.81-20.474-7.811-28.284,0L40,443.716v-99.503c0-11.046-8.954-20-20-20s-20,8.954-20,20V492    c0,10.866,8.853,20,20,20h147.788c11.046,0,20-8.954,20-20c0-11.046-8.954-20-20-20H68.284l160.322-160.322    C236.417,303.867,236.417,291.204,228.606,283.394z"/>
+            </g>
+          </g>
+          </svg>        
         </div>
       </div>
     </div>
@@ -77,26 +99,59 @@ export default {
         let html = document.documentElement;
         html.setAttribute('style', `font-size:${localStorage.getItem('fontSize')}px !important`);
       }
+      if(localStorage.getItem('maxWidth')){
+        let documentWrapper = document.getElementsByClassName('documentwrapper')[0];
+        documentWrapper.setAttribute('style', `max-width: ${localStorage.getItem('maxWidth')}px`)
+      }
     },
     getGlobalFontSize: function (){
       let html = document.documentElement;
       let fontSize = window.getComputedStyle(html).fontSize;
       // we only want the number so we remove the rest
       fontSize = fontSize.replace('!important', '').replace(/\s+/, '').replace('px', '')
-      console.log(fontSize);
       return parseInt(fontSize)
+    },
+    getDocumentSize: function (){
+      let documentWrapper = document.getElementsByClassName('documentwrapper')[0];
+      let maxWidth = window.getComputedStyle(documentWrapper).maxWidth.replace('!important', '').replace(/\s+/, '').replace('px', '');
+      return parseInt(maxWidth);
+    },
+    increaseDocumentSize: function (){ 
+      let documentWrapper = document.getElementsByClassName('documentwrapper')[0];
+      let maxWidth = this.getDocumentSize();
+      
+      
+      // on large screens set max bounding to 1200
+      // on smaller screens set to 840
+      if(window.innerWidth > 2000){
+        maxWidth = Math.min(maxWidth + 100, 1200);
+      }else{
+        maxWidth = Math.min(maxWidth + 100, 840);
+      }
+      
+      documentWrapper.setAttribute('style', `max-width: ${maxWidth}px`)
+      localStorage.setItem('maxWidth', maxWidth)
+    },
+    decreaseDocumentSize: function (){
+      let documentWrapper = document.getElementsByClassName('documentwrapper')[0];
+      let maxWidth = this.getDocumentSize();
+
+      // setting min bound to 400
+      maxWidth = Math.max(maxWidth - 100, 400);
+      documentWrapper.setAttribute('style', `max-width: ${maxWidth}px`)
+      localStorage.setItem('maxWidth', maxWidth)
     },
     increaseFontSize: function (){
       let fontSize = this.getGlobalFontSize();
       let html = document.documentElement;
-      fontSize = fontSize + 1;
+      fontSize = Math.min(fontSize + 1, 15);
       html.setAttribute('style', `font-size:${fontSize}px !important`);
       localStorage.setItem('fontSize', fontSize)
     },
     decreaseFontSize: function (){
       let fontSize = this.getGlobalFontSize();
       let html = document.documentElement;
-      fontSize = fontSize - 1;
+      fontSize = Math.max(fontSize - 1, 8);
       html.setAttribute('style', `font-size:${fontSize}px !important`);
       localStorage.setItem('fontSize', fontSize)
     },
